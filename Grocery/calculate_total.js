b79a3a9cfb714e8dc501d1311d6ea7a0d0c9173c
“Howard <higamelsa430@gmail.com>
2025-12-13 23:06:38 -0500

    Grocery Task

---
+++ Grocery/calculate_total.js
@@ -0,0 +1,13 @@
+let grocery1;
+let grocery2;
+let grocery3;
+
+
+function calculateTotal() {
+    grocery1 = parseFloat(document.getElementById('g1').value);
+    grocery2 = parseFloat(document.getElementById('g2').value);
+    grocery3 = parseFloat(document.getElementById('g3').value);
+
+    let total = grocery1 + grocery2 +grocery3;
+    document.getElementById('result').innerText = `The total amount is: $${area}`;
+}

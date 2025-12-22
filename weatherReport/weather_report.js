d04a5de5f4557a0f8503775d81feff7757d2280d
“Howard <higamelsa430@gmail.com>
2025-12-21 23:28:03 -0500

    Task weather api

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,12 +1,13 @@
 function showweatherDetails(event) {
     event.preventDefault();
-    const city = document.getElementById('city').value;
-    const apiKey = 'YOUR_API_KEY';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = 'your_api_key';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const weatherInfo = documebnt.getElementById('weatherInfo');
+            const weatherInfo = document.getElementById('weatherInfo');
             weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
             <p>Temperature: ${data.main.temp} &#8451;</p>
             <p>Weather: ${data.weather[0].description}</p>`;
